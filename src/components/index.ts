27ed4560f34b33f956f72bcef7cae882de79a78b
Geider <garevaloqq@gmail.com>
2022-12-12 11:28:41 -0300

    Add NotFoundProduct component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Breadcrumb } from './Breadcrumb';
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as NotFoundPage } from './NotFoundPage';
+export { default as NotFoundProduct } from './ui/NotFoundProduct';
 export { default as PageLoader } from './PageLoader';
 export { default as PriceView } from './PriceView';
 export { default as CardProduct } from './ui/CardProduct';
